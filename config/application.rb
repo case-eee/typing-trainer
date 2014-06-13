71784a346d576826f52892b8985b5aee3da31fcc
Casey Ann Cumbow <cumbow8@gmail.com>
2014-06-13 10:17:13 -0500

    heroku

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,6 @@ Bundler.require(*Rails.groups)
 module Blog
   class Application < Rails::Application
     config.assets.paths << Rails.root.join("templates")
-    config.serve_static_assets = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
