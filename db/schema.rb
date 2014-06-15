a96a67c6cd8a37f17a68a9730856aa51df7c1a01
Gustavo Puglia <gpugliac@gmail.com>
2014-06-14 20:48:18 -0500

    Adds devise gem and initial config

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140613195200) do
+ActiveRecord::Schema.define(version: 20140615012535) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,11 +42,14 @@ ActiveRecord::Schema.define(version: 20140613195200) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
-    t.text     "contents"
-    t.string   "author"
-    t.datetime "post_date"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts_tags", id: false, force: true do |t|
+    t.integer "post_id"
+    t.integer "tag_id"
   end
 
   create_table "scripts", force: true do |t|
@@ -57,6 +60,12 @@ ActiveRecord::Schema.define(version: 20140613195200) do
     t.datetime "updated_at"
   end
 
+  create_table "tags", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "tracks", force: true do |t|
     t.integer  "user_id"
     t.integer  "lesson_id"
@@ -65,11 +74,21 @@ ActiveRecord::Schema.define(version: 20140613195200) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "username"
-    t.string   "email"
-    t.string   "password"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
