8899ab847cb360220ff2e90d119c19cade4aaebe
Casey Ann Cumbow <cumbow8@gmail.com>
2014-06-15 14:29:41 -0500

    progress with lesson controller testing

---
+++ spec/controllers/lessons_controller_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe LessonsController do
+	before do
+		@lesson1 = Lesson.create(difficulty_level: 1, language_id: 5)
+		@lesson2 = Lesson.create(difficulty_level: 2, language_id: 1)
+	end
+
+	describe '#index'
+
+		before do 
+			get :index, :format => :json
+		end
+
+		it "should retrieve a content-type of json" do
+			expect(response.header['Content-Type']).to include "application/json"
+		end
+
+		it "should retrieve status code of 200" do
+			expect(response.response_code).to eq 200
+		end
+
+	end
+
